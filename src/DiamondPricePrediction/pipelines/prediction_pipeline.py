258e0906d9f96fe1e9e355d195e3419cecc2338b
nitin kumar <nitin6201153107@gmail.com>
2024-02-26 12:53:43 +0530

    implemented load_object method in utils.py file

---
+++ src/DiamondPricePrediction/pipelines/prediction_pipeline.py
@@ -0,0 +1,6 @@
+ import pandas as pd
+ import sys
+ import os
+ from src.DiamondPricePrediction.exception import CustomException
+ from src.DiamondPricePrediction.logger import logging
+ from src.DiamondPricePrediction.utils.utils import load_object
---
+++ src/DiamondPricePrediction/pipelines/trainer_pipeline.py
@@ -0,0 +1,21 @@
+from src.DiamondPricePrediction.components.data_ingestion import DataIngestion
+
+from src.DiamondPricePrediction.components.data_transformation import DataTransformation
+
+from src.DiamondPricePrediction.components.model_trainer import ModelTrainer
+
+
+
+
+import os
+import sys
+from src.DiamondPricePrediction.logger import logging
+from src.DiamondPricePrediction.exception import CustomException
+import pandas as pd
+data_ingestion=DataIngestion()
+train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+                        
+data_transformation = DataTransformation()
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+model_trainer=ModelTrainer()
+model_trainer.initate_model_training(train_arr,test_arr)
