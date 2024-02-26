a0766d31cfa07391b4e4f4ead3919d876acb7216
nitin kumar <nitin6201153107@gmail.com>
2024-02-26 18:27:02 +0530

    added 8080 port in app.py file

--- app.py
+++ app.py
@@ -37,5 +37,6 @@ def predict_datapoint():
         
         return render_template("result.html",final_result=result)
 
-#if __name__=="main":
-app.run()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
